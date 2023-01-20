909cf2db7d2bbacfeb254f9a381d8b2eb658b2da
rcholland <christophaw@hotmail.com>
2023-01-20 16:01:46 +0200

    created scripts

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    const string xAxis = "Mouse X";
+    const string yAxis = "Mouse Y";
+
+    [SerializeField]
+    private GameObject mainCamera;
+
+    public float mouseSensitivity = 500.0f;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis(xAxis) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(yAxis) * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        Mathf.Clamp(xRotation, -60.0f, 60.0f);
+        mainCamera.transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
+
 {
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 10.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(horizontal * speed * Vector3.right * Time.deltaTime);
+
+        transform.Translate(vertical * speed * Vector3.forward * Time.deltaTime);
     }
 }
+  //left right moving
+    
+
