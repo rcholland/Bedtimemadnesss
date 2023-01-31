2378b7d41b39631dc2b38374bfb8eef7d20b7b8d
rcholland <christophaw@hotmail.com>
2023-01-31 15:04:41 +0000

    added baby and animations controllers

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+
+    [SerializeField] Animator animator;
+    float horizontal;
+    float vertical;
+
+    bool shooting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        UpdateAnimations(0.0f, 0.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        UpdateAnimations(horizontal, vertical);
+        if (Input.GetButtonDown("Fire1"))shoot();
+        else if (Input.GetButtonUp("Fire1"))shoot();
+
+    }
+    public void UpdateAnimations(float h, float v)
+    {
+        animator.SetFloat("horizontal",h);
+        animator.SetFloat("vertical", v);
+    }
+
+void shoot()
+    {
+        shooting = !shooting;
+        animator.SetBool("shooting", shooting);
+
+    }
+}
