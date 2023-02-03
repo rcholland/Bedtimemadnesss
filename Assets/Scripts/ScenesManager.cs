fd39148e421bf21df30fab5d3b0682a2708f7a90
rcholland <christophaw@hotmail.com>
2023-02-03 11:06:00 +0000

    Scenes management script

--- Assets/Scripts/ScenesManager.cs
+++ Assets/Scripts/ScenesManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class ScenesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public enum Scenes
     {
-        
+        bootup,
+        title,
+        waveOne,
+        waveTwo,
+        waveThree,
+        waveBoss,
+        gameOver,
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BeginGame()
     {
-        
+        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(1,LoadSceneMode.Additive);
     }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void MainScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
