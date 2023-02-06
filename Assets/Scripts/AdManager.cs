4344f68dbab61e002dfd5687eabc284f8c76ff66
rcholland <christophaw@hotmail.com>
2023-02-06 10:46:04 +0000

    added ad management

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour
+{
+
+    [SerializeField] string androidGameId;
+    [SerializeField] string iOSGameId;
+    [SerializeField] bool testMode= true;
+    string adId = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
